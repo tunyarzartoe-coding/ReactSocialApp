ffb919ab4ccba84e4484ff9cb1f7004649d5c908
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-29 17:00:42 +0630

    [Complete] main loading

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { store } from "./store/store";
 import "react-toastify/dist/ReactToastify.css";
 
 import { getAllPosts } from "./components/posts/postSlice";
-import {  getAllUsers } from "./components/users/userSlice";
+import { getAllUsers } from "./components/users/userSlice";
+import MainLoading from "./tools/MainLoading";
 
-store.dispatch(getAllPosts())
-store.dispatch(getAllUsers())
+store.dispatch(getAllPosts());
+store.dispatch(getAllUsers());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+        <Suspense fallback={<MainLoading />}>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Provider>
   </React.StrictMode>
---
+++ src/tools/MainLoading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainLoading = () => {
+  return <div className="spinner"></div>;
+};
+
+export default MainLoading;
