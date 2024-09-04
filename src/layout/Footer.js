d7ececf1a937abacd1a5c778875ffb6af559cf64
htunyarzar80 <htunyarzar2001@gmail.com>
2024-09-04 15:38:25 +0630

    [fixing] footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-text">
         {/* <p> */}
-          &copy; {new Date().getFullYear()} | Tun Yar Zar Toe. All rights
-          reserved.
+        &copy; {new Date().getFullYear()} |
+        <Link
+          className="px-1 footer-text"
+          href="https://github.com/tunyarzartoe-coding"
+          target="_blank"
+          // rel="noopener noreferrer"
+          // className="footer-link"
+        >
+          Tun Yar Zar Toe
+        </Link>
+        . All rights reserved.
         {/* </p> */}
       </div>
     </footer>
